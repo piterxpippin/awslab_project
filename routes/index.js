1f0146fca7367c152ec1a503df32c1f9ab5ee087
Piotr Pawlak <pieter.pippin@gmail.com>
2016-02-16 23:35:42 +0100

    Photo gallery

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var AWS = require('aws-sdk');
 var H = require('../helpers/helpers.js');
 var PORT = 3000;
 
-H.getPar();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var s3Credentials = H.generateS3Credentials();
@@ -23,6 +21,35 @@ router.get('/', function(req, res, next) {
 router.get('/logEvent', function(req, res, next) {
     H.logUpload();
     res.redirect('/');
+});
+
+router.get('/pictureGallery', function(req, res, next) {
+    var s3 = new AWS.S3();
+    var params = {
+        Bucket: 'pawlak-aws-project', /* required */
+        Delimiter: ';',
+        EncodingType: 'url',
+        Marker: 'AA',
+        MaxKeys: 1000,
+        Prefix: 'piotr.pawlak/'
+    };
+
+    var contents = Array();
+    var root = 'https://s3-us-west-2.amazonaws.com/pawlak-aws-project/';
+
+    s3.listObjects(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            for (var i = 1; i < data.Contents.length; i++) {
+                contents.push(root + data.Contents[i].Key);
+            }
+
+            res.render('pictureGallery', {
+                imageContent: contents, 
+                title: 'Picture Gallery'
+            });
+        }    
+    });
 });
 
 module.exports = router;
